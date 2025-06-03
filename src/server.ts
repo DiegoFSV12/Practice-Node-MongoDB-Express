5c35c4009f954eb3f8c5b4458d7b73bf556b20f8
DiegoFSV12 <dsanchezver@ucvvirtual.edu.pe>
2025-06-02 20:26:11 -0500

    feat: update server.ts

--- src/server.ts
+++ src/server.ts
@@ -2,16 +2,22 @@ import 'dotenv/config';
 import express from 'express';
 import routesMascotas from "./routes/mascotas";
 import bodyParser from 'body-parser';
+import dbClient from './config/dbClient';
 
 export class Server{
     private app = express();
     async start(PORT:number){
-        this.app.listen(PORT || 3000,()=>{
+        this.app.listen(process.env.PORT || 3000,()=>{
             console.log(`Conectado al puerto ${PORT}`);
         })
 
         this.app.use(bodyParser.json());
         this.app.use(bodyParser.urlencoded({extended:true}));
         this.app.use("/mascotas", routesMascotas);
+
+        process.on('SIGINT',async()=>{
+            dbClient.disconnect();
+            process.exit(0);
+        })
     }
 }
