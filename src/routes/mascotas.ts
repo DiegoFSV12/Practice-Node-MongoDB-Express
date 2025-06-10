1c4c19b20af61a57e0c483e9830bca956f6ef77a
DiegoFSV12 <dsanchezver@ucvvirtual.edu.pe>
2025-06-09 22:11:52 -0500

    feat: Update to validateToken

--- src/routes/mascotas.ts
+++ src/routes/mascotas.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import mascotaController from "../controllers/mascotas";
+import { validateToken } from "../helpers/autenticacion";
 
 export const routesMascotas = express.Router();
 
 routesMascotas.get("/",mascotaController.getAll);
 routesMascotas.get("/:id",mascotaController.getOne);
-routesMascotas.post("/",mascotaController.create);
-routesMascotas.put("/:id",mascotaController.update);
-routesMascotas.delete("/:id",mascotaController.delete);
+routesMascotas.post("/",validateToken,mascotaController.create);
+routesMascotas.put("/:id",validateToken,mascotaController.update);
+routesMascotas.delete("/:id",validateToken,mascotaController.delete);
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import usuarioController from "../controllers/users";
+import { validateToken } from "../helpers/autenticacion";
 
 export const routesUsers = express.Router();
 
 routesUsers.post('/register', usuarioController.register);
 routesUsers.post('/login', usuarioController.login);
-
+routesUsers.get('/profile', validateToken, usuarioController.getProfile);
