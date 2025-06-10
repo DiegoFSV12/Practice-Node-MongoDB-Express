54872d68352cdd1f6f2b83041796663f7f4c31b6
DiegoFSV12 <dsanchezver@ucvvirtual.edu.pe>
2025-06-09 22:11:26 -0500

    feat: validateToken

--- src/helpers/autenticacion.ts
+++ src/helpers/autenticacion.ts
@@ -1,8 +1,36 @@
 import 'dotenv/config';
+import { NextFunction, Request, Response } from 'express';
 import jsonwebtoken from 'jsonwebtoken'
 
-function generateToken(email:string){
+declare global {
+  namespace Express {
+    interface Request {
+      emailConected?: string;
+    }
+  }
+}
+
+export function generateToken(email:string){
     return jsonwebtoken.sign({email},`${process.env.JWT_SECRET}`,{expiresIn: '1hr'});
 }
 
-export {generateToken}
+export function validateToken(req:Request, res:Response, next:NextFunction){
+    const token = req.header('Authorization')?.replace('Bearer ','');
+    if(!token){
+        res.status(401).json({error:"Token requerido"});
+        return;
+    }
+    try {
+        const datatoken = jsonwebtoken.verify(token,`${process.env.JWT_SECRET}`);
+        if(typeof(datatoken) == "string"){
+            res.status(401).json({error:"Token no valido"});
+            return;
+        }
+        console.log(datatoken.email);
+        req.emailConected = datatoken.email;
+        next(); //Esto hace que tras ejecutar la validación, pase a la siguiente función
+    } catch (error) {
+        res.status(401).json({error:"Token invalido"});
+        return;
+    }
+}
